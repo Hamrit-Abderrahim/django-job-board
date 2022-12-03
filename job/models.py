c122bf93bba32c560c00ae5a97b1d70f81160559
hamrit abd errahim <escobarp248@gmail.com>
2022-12-03 07:11:12 +0100

    accounts

--- job/models.py
+++ job/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+JOB_TYPE = (
+    ('PART TIME', 'PART TIME'),
+    ('FULL TIME', 'FULL TIME'),
+)
+
+
+def upload_img(instance, fileName):
+    imgName, extention = fileName.split(".")
+    return "jobs/%s_%s.%s" % (instance.title, instance.id, extention)
+
+
+class Job(models.Model):  # table
+    owner = models.ForeignKey(
+        User, related_name='job_owner', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)  # column
+    job_type = models.CharField(max_length=20, choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=upload_img)
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Job, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Apply(models.Model):
+    job = models.ForeignKey(
+        Job, related_name='apply_job', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    website = models.URLField()
+    cv = models.FileField(upload_to='apply/')
+    cover_letter = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- job/views.py
+++ job/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .models import Job
+from django.core.paginator import Paginator
+from .form import ApplyForm, JobForm
 
-# Create your views here.
+
+# Create your views here.s
+
+
+def job_list(request):
+    job_list = Job.objects.all()
+    paginator = Paginator(job_list, 3)  # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {'jobs': page_obj}
+    return render(request, 'job/jobs_list.html', context)
+
+
+def job_detail(request, slug):
+    job_detail = Job.objects.get(slug=slug)
+    if request.method == 'POST':
+        form = ApplyForm(request.POST, request.FILES)
+        if form.is_valid():
+            myForm = form.save(commit=False)
+            myForm.job = job_detail
+            myForm = form.save()
+    else:
+        form = ApplyForm()
+
+    context = {'job': job_detail, 'form': form}
+
+    return render(request, 'job/job_details.html', context)
+
+
+def add_job(request):
+
+    if request.method == 'POST':
+        form = JobForm(request.POST, request.FILES)
+        if form.is_valid():
+            myForm = form.save(commit=False)
+            myForm.owner = request.user
+            myForm.save(
+
+            )
+            return redirect(reverse('jobs:job_list'))
+    else:
+        form = JobForm()
+
+    return render(request, 'job/add_job.html', {'form1': form})
